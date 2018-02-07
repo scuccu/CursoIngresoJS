921695a1bdf4bf61282cbf646f4dd026180cf030
scuccu <36194059+scuccu@users.noreply.github.com>
2018-02-07 11:47:06 -0300

    Ejercicio 1,2y3
    If y else

---
+++ jsInstruccionIF (2).js
@@ -0,0 +1,14 @@
+function Mostrar()
+{
+//tomo la edad  
+
+var edad;
+
+edad=document.getElementById('edad').value;
+
+if(edad>17)
+{
+	alert("La persona es mayor de edad");
+}
+
+}//FIN DE LA FUNCIÓN
---
+++ jsInstruccionIF (3).js
@@ -0,0 +1,19 @@
+function Mostrar()
+{
+//tomo la edad  
+
+var edad
+
+edad=document.getElementById('edad').value;
+
+if(edad>17)
+{
+	alert("La persona es mayor de edad");
+}
+
+if(edad<18)
+{
+	alert("La persona es menor de edad");
+}
+
+}//FIN DE LA FUNCIÓN
