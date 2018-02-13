25a3e5319474e69c668e9feae31e2506ca5c1773
scuccu <36194059+scuccu@users.noreply.github.com>
2018-02-13 12:35:56 -0300

    Ejercicios 3 y 4

--- jsInstruccionIF (4).js
+++ jsInstruccionIF (4).js
@@ -1,29 +1,29 @@
 function Mostrar()
 {
 //tomo la edad  
+	
 	var edad;
-
 	edad=document.getElementById('edad').value;
 	edad=parseInt(edad);
 
 	if(edad<=0)
-		{
-			alert("Ingrese bien la edad");
-		}
-
-		else
-		{
-			if(edad>=18)
 	{
-		alert("Mayor");
+		alert("Ingrese una edad");
 	}
+
 	else
 	{
-		alert("Menor");
-	}
-		
+		if(edad>=13&&edad<=17)
+		{
+			alert("Adolescente");
+		}
+		else
+		{
+			alert("No es adolescente")
+		}
 
 	}
-	
+
+
 
 }//FIN DE LA FUNCIÓN
