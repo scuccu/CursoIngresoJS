edbfa08667bcae50e862c5758bbdd7ff81a1440e
scuccu <36194059+scuccu@users.noreply.github.com>
2018-02-13 12:58:18 -0300

    Ejercicios 5 y 6 (El 5 no esta corregido)

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,23 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad;
 
+	edad=document.getElementById('edad').value;
+	edad=parseInt(edad);
+
+	if(edad<=0)
+		{
+			alert("Ingrese bien la edad");
+		}
+
+		else
+		{
+			if(!(edad>=13||edad<=17);
+			{
+				alert("No es adolescente");
+			}
+			
+		}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -2,7 +2,20 @@ function Mostrar()
 {
 //tomo la edad  
 
+	var edad;
+	edad=document.getElementById('edad').value;
+	edad=parseInt(edad);
+	if(edad<=12)
+	{
+		alert("niño");
+	}
+	if(edad<=17)
+	{
+		alert("Adolescente");
+	}
 
-
-
+	if(edad>=18)
+	{
+		alert("Mayor de edad");
+	}
 }//FIN DE LA FUNCIÓN
