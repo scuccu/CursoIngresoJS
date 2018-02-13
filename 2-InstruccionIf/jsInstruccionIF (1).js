595509838697d877abab7148e1b2f916bd7eff54
scuccu <36194059+scuccu@users.noreply.github.com>
2018-02-13 12:02:18 -0300

    Ejercicio 1

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,7 +1,19 @@
 function Mostrar()
 {
 //tomo la edad  
+	var edad;
+	edad=document.getElementById('edad').value;
+	edad=parseInt(edad);
 
+	if(edad==15)
+	{
+		alert("niña bonita");
+
+	}
+	else
+	{
+		alert("nada");
+	}
 
 
 }//FIN DE LA FUNCIÓN
