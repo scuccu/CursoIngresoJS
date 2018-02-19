d7dbd3ace22fc233746e88ce7e7fb55d59a67cbe
scuccu <36194059+scuccu@users.noreply.github.com>
2018-02-19 15:54:05 -0300

    Ejercicios 1, 2, 3 y 4

--- 9-Parciales/parcial/cuatro.js
+++ 9-Parciales/parcial/cuatro.js
@@ -1,4 +1,34 @@
 function Mostrar()
 {
+ 	var numeroUno
+ 	var numeroDos
+ 	var resultado
 
-}
+ 	numeroUno=prompt("Ingrese aquí un número")
+ 	numeroDos=prompt("Ingrese aquí otro número")
+ 	numeroUno=parseInt(numeroUno);
+ 	numeroDos=parseInt(numeroDos);
+
+ 	if(numeroUno==numeroDos)
+ 		{
+ 			resultado=numeroUno*numeroDos;
+
+ 			document.write("Su resultado es "+resultado);
+ 		}
+
+ 	if(numeroUno>numeroDos)
+ 	{
+ 		resultado=numeroUno-numeroDos;
+ 		document.write("Su resultado es "+resultado);
+ 	}
+ 		
+	
+	if(numeroUno<numeroDos)
+	{
+	 	resultado=numeroUno+numeroDos;
+ 		document.write("Su resultado es "+resultado);
+	}
+
+ 	
+ 		
+}	
--- 9-Parciales/parcial/tres.js
+++ 9-Parciales/parcial/tres.js
@@ -1,4 +1,18 @@
 function Mostrar()
 {
+	var largo;
+	var ancho;
+	var alambre;
+	var perimetro;
 
+	largo=document.getElementById('alrgo').value;
+	ancho=document.getElementById('ancho').value;
+
+	largo=parseInt(largo);
+	ancho=parseInt(ancho);
+
+	perimetro=largo*ancho;
+	alambre=perimetro*3;
+
+	alert("Usted necesita "+alambre+" mts");
 }
