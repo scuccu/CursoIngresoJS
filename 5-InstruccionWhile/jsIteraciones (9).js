637d4986888ba543b4f2657140e32ed573fc4291
scuccu <36194059+scuccu@users.noreply.github.com>
2018-02-20 12:02:20 -0300

    Ejercicio 9

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -2,13 +2,32 @@ function Mostrar()
 {
 
 	var contador=0;
-	// declarar variables
+	var maximo;
+	var minimo;
+	var numero;
 	
 	var respuesta='si';
 
 	while(respuesta!='no')
 	{
-		
+		contador++;
+		numero=prompt("");
+		numero=parseInt(numero);
+		if(contador==1)
+		{
+			maximo=numero;
+			minimo=numero;
+		}else
+		{
+			if(numero>maximo)
+			{
+				maximo=numero;
+			}
+			if(numero<minimo)
+			{
+				minimo= numero
+			}
+		}
 	
 	}
 
