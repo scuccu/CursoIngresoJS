3f7ded65b7e2324f2204fbe2447568927cd17e6d
scuccu <36194059+scuccu@users.noreply.github.com>
2018-02-21 10:33:07 -0300

    Ejercicio 1 (par, impar, ceros, positivos y negativos

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,53 @@
 function Mostrar()
 {
+	//var contador=0
+	var iteraciones=10;
+	var contadorDePar=0
+	var contadorDeImpar=0
+	var contadordeceros=0
+	var contadorDePositivos=0
+	var contadorDeNegativos=0
+	
+
+
+	for(contador=-10;contador<iteraciones;contador++)
+	{
+		//document.write("<br>"+contador);
+		if(contador==0)
+		{
+			contadordeceros++;
+		}
+		else
+		{
+			if(contador%2==0)
+			{
+				contadorDePar++;
+			}
+		}
+		if(!(contador%2==0))
+		{
+			contadorDeImpar++;
+		}
+		
+		//Positivos y negativos
+
+		if(contador>=1)
+		{
+			contadorDePositivos++
+		}
+		else
+		{
+			if(contador<=-1)
+			{
+				contadorDeNegativos++
+			}
+		}
+	}
+		//document.write("<br>"+contador);
+		document.write("<br> par: "+contadorDePar);
+		document.write("<br> impar: "+contadorDeImpar);
+		document.write("<br> ceros: "+contadordeceros);
+		document.write("<br> positivos: "+contadorDePositivos);
+		document.write("<br> negativos: "+contadorDeNegativos);
 
 }
