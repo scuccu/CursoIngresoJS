ecb17a5960b0d7899244db92fd452fafa9586cef
scuccu <36194059+scuccu@users.noreply.github.com>
2018-02-27 20:49:12 -0300

    parciales 7 y 6 de la lista

--- 9-Parciales/parcial/seis.js
+++ 9-Parciales/parcial/seis.js
@@ -1,4 +1,33 @@
 function Mostrar()
 {
+	var importe;
+	var contador=0;
+	var iteraciones=24;
+	var maximo=0;
+	var minimo=100;
+
+	while(contador<iteraciones)
+	{
+		contador++;
+		importe=prompt("Ingrese su importe aquí");
+		importe=parseInt(importe);
+
+		if(importe<0)
+		{
+			importe=prompt("Indique un importe correcto");
+			importe=prompt(importe);
+		}
+		if(importe>maximo)
+		{
+			maximo=importe;
+
+		}
+		if(importe<minimo)
+		{
+			minimo=importe;
+		}
+	}
+	alert("El mayor importe fue: "+maximo+". Y el menor importe fue "+minimo);
+
 
 }
--- 9-Parciales/parcial/siete.js
+++ 9-Parciales/parcial/siete.js
@@ -1,4 +1,49 @@
 function Mostrar()
 {
+	var notas;
+	var contador=0;
+	var acumulador=0;
+	var iteraciones=10;
+	var sexo;
+	var promedio;
+	var minimo=100;
+	var varonesmayores=0;
+
+	while(contador<iteraciones)
+	{
+		contador++;
+		notas=prompt("Ingrese aquí una nota");
+		notas=parseInt(notas);
+
+		if(notas<0 || notas>10)
+		{
+			notas=prompt("Ingrese una nota correcta");
+			notas=parseInt(notas);
+		}
+		if(notas<minimo)
+		{
+			minimo=notas;
+		}
+
+		sexo=prompt("Ingrese su sexo f/m")
+		
+		while(!(sexo=="f" || sexo=="m"))
+		{
+			sexo=prompt("Indique su sexo correcto")
+		}
+		
+		if(sexo=="m" && notas>=6)
+		{
+			varonesmayores++;
+		}
+		
+		acumulador=acumulador+notas;
+		promedio=acumulador/contador;
+
+	}
+	document.write("<br> El promedio de notas totales: "+promedio);
+	document.write("<br> La nota más baja: "+minimo);
+	document.write("<br> La cantidad de varonesque su nota haya sido + o = a 6: "+varonesmayores)
+
 
 }
