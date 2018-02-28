b150e1582c72aea44ca7226e50714888105c33e5
scuccu <36194059+scuccu@users.noreply.github.com>
2018-02-28 02:06:29 -0300

    Ejercicio 7 segundo parcial arreglar

--- 9-Parciales/parcial2/7-iteraciones.js
+++ 9-Parciales/parcial2/7-iteraciones.js
@@ -1,7 +1,55 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 7-iteraciones");
-	
+	var notas;
+	var iteracionesN=10;
+	var iteracionesS=6;
+	var acumulador=0;
+	var contadorN=0;
+	var contadorS=0;
+	var cantV=0;
+	var minimo=100;
+	var sexo;
+
+	while(contadorN<iteracionesN)
+	{
+		contadorN++;
+		notas=prompt("Ingrese una nota");
+		notas=parseInt(notas);
+
+		if(notas<0 || notas>10 || isNaN(notas))
+		{
+			notas=prompt("Ingrese una nota real");
+			notas=parseInt(notas);
+		}
+		
+		while(contadorS<iteracionesS)
+		{
+			contadorS++;
+			sexo=prompt("Ingrese un sexo f/m");
+		}
+		
+		if(!(sexo=="f" || sexo=="m"))
+		{
+			sexo=prompt("Ingrese un sexo correcto");
+		}
+		if(notas<minimo)
+		{
+			minimo=notas;
+		}
+		if(sexo=="m" && notas<=6)
+		{
+			cantV++;
+		}
+
+
+		acumulador=acumulador+notas;
+		promedio=acumulador/contadorN;
+	}
+
+	document.write("<br> El promedio de notas totales es: "+promedio);
+	document.write("<br> El minimo de notas es: "+minimo);
+	document.write("<br> Varones cuyas notas son iguales o mayores a 6: "+cantV);
+
 }
 
