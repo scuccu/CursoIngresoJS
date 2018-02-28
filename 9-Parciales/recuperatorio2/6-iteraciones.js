8aae8ddbf9ba80e4ea048758739f4f5aaeab8c49
scuccu <36194059+scuccu@users.noreply.github.com>
2018-02-28 01:48:51 -0300

    Ejercicio 6 del segundo parcial

--- 9-Parciales/recuperatorio2/6-iteraciones.js
+++ 9-Parciales/recuperatorio2/6-iteraciones.js
@@ -1,7 +1,35 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 6-iteraciones");
+	var importe;
+	var iteraciones=7;
+	var contador=0;
+	var maximo=0;
+	var minimo=100;
+
+	while(contador<iteraciones)
+	{
+		contador++;
+		importe=prompt("Ingrese un importe");
+		importe=parseInt(importe);
+
+		while(importe<=0)
+		{
+			importe=prompt("Ingrese un importe correcto");
+			importe=parseInt(importe);
+		}
+		if(importe>maximo)
+		{
+			maximo=importe;
+		}
+		if(importe<minimo)
+		{
+			minimo=importe
+		}
+	}
+	document.write("<br> El importe más alto fue: "+maximo);
+	document.write("<br> El importe más bajo fue: "+minimo);
+
 	
 }
 
