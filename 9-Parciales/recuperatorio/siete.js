6b86b36ceddf74bd1f39749c8be952da5d074d2a
scuccu <36194059+scuccu@users.noreply.github.com>
2018-02-28 01:07:11 -0300

    Ejercicio 7 del primer recuperatorio

--- 9-Parciales/recuperatorio/siete.js
+++ 9-Parciales/recuperatorio/siete.js
@@ -1,4 +1,56 @@
 function Mostrar()
 {
 
+	var edad;
+	var iteraciones=100;
+	var contador=0;
+	var sexo;
+	var cantv=0;
+	var promedioEdad;
+	var acumuladorEdad=0;
+	var minimo=100;
+
+	while(contador<iteraciones)
+	{
+			contador++;
+			edad=prompt("Ingrese una edad");
+			edad=parseInt(edad);
+			
+		
+		while(edad<0 || edad>100 || isNaN(edad))
+		
+		{
+			edad=prompt("Ingrese una edad correcta");
+			edad=parseInt(edad);
+		}
+		
+		sexo=prompt("Ingrese un sexo correcto");
+		
+		while(!(sexo=="f" || sexo=="m"))
+		{
+			sexo=prompt("Ingrese un sexo correcto");
+
+		}
+		
+		if(edad<minimo)
+		{
+			minimo=edad;
+		}
+		
+		
+		if(sexo=="m" && edad>=20)
+		{
+			cantv++;
+		}
+
+		acumuladorEdad=acumuladorEdad+edad;
+		promedioEdad=acumuladorEdad/contador;
+	}
+
+
+	document.write("<br> El promedio de la edad es: "+promedioEdad);
+	document.write("<br> La edad más baja es: "+minimo);
+	document.write("<br> Cantidad de varones mayores o iguales a 20: "+cantv);
+
+
 }
