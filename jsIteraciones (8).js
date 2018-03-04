cde25ef6e50d1871f01466ddb263fbf137d2d6c8
scuccu <36194059+scuccu@users.noreply.github.com>
2018-03-03 21:09:19 -0300

    ejercicio con animales

--- jsIteraciones (8).js
+++ jsIteraciones (8).js
@@ -1,115 +1,147 @@
+
 //se ingresa hasta que el usuario quiera el nombre del animal, su peso, validar que sea mayor a 0, y la temperatura del habitad de este animal
 //validar entre 40 y menos 40. Informar la cantidad de temperaturas pares, impares, el nombre del animal más pesado, el nombre del animal menos pesado
 // la cantidad de animales que viven en habitad menores a -0 inclusive el promedio del peso de todos los animales, la temp maxima y minima
 
+
 function Mostrar()
 {
-	var nombre;
-	var nombremaximoP;
-	var nombreminimoP;
-	var interaciones=0;
-	var contadorA=0;
-	var temperaturamax=0;
-	var temperaturamin=0;
-	var animalesFrios=0;
+
+	var animal;
 	var peso;
-	var contadorpeso=0;
-	var acumuladorPeso=0;
-	var promedioPeso;
-	var maximoP=0;
-	var minimoP=0;
-	var maximoT=0;
-	var minimoT=0;
+	var contadorPeso=0;
 	var temperatura;
 	var temperaturaPar=0;
 	var temperaturaImpar=0;
 	var respuesta="si";
-	
+	var animalPesado;
+	var animalLiviano;
+	var animalFrio=0;
+	var temperaturaMaxima;
+	var temperaturaMinima;
+	var contadorTemperatura=0;
+	var contadorAnimal=0;
+	var iteraciones=1;
+	var acumuladorPeso;
+	var promedioPeso;
+	var maximo;
+	var minimo;
+	var pesomaximo;
+	var pesominimo;
+
+
 	while(respuesta=="si")
 	{
-		contador++;
-		nombre=prompt("Ingrese un animal");
+		contadorAnimal++;
+		animal=prompt("Ingrese el nombre del animal");
 
-		contadorpeso++;
-		peso=("ingrese el epso del animal");
+		contadorPeso++;
+		peso=prompt("Ingrese un peso aquí");
+		peso=parseInt(peso);
 
-		while(peso<=0)
+		if(peso<=0 || isNaN(peso))
 		{
-			peso=prompt("Ingrese un peso correcto");
+			peso=prompt("Ingrese un peso correcto por favor");
 			peso=parseInt(peso);
 		}
 
-		temperatura=("Ingrese la temperatura");
+		contadorTemperatura++;
+		temperatura=prompt("Ingrese una temperatura");
+		temperatura=parseInt(temperatura);
 
-		while(!(temperatura>-40 || temperatura<40 || temperatura=0))
+		if(temperatura>40 || temperatura<-40 || isNaN(peso))
 		{
 			temperatura=prompt("Ingrese una temperatura correcta");
 			temperatura=parseInt(temperatura);
 		}
 
-		
-
-		if(temperatura%2==0 && temperatura!=0)
+		if(temperatura%2==0)
 		{
 			temperaturaPar++;
 		}
 		else
+		{
+			if(!(temperatura%2==0))
 			{
 				temperaturaImpar++;
 			}
-
-		if(interaciones==1)
-		{
-			maximoP=peso;
-			minimoP=peso;
-			nombremaximoP=nombre;
-			nombreminimoP=nombre;
-			temperaturamax=temperatura;
-			temperaturamin=temperatura;
 		}
-		else if(maximoP>peso)
-		   	{
-		   		nombremaximoP=maximoP;
-			}	
+		
+		//animal mas pesado y mas liviano
 
-			if(minimoP<peso)
-			{
-				nombreminimoP=minimoP;
-			}
-
-			if(temperaturamax>temperatura)
-			{
-				temperaturamax=temperatura;
-			}
-			if(temperaturamin<temperatura)
-			{
-				temperaturamin=temperatura;
-			}
-
-		if(temperatura<=-0)
+		//temperatura maxima y minimo
+		if(contadorAnimal==1)
 		{
-			animalesfrios++;
+			pesominimo=peso;
+			pesomaximo=peso;
+			animalPesado=animal;
+			animalLiviano=animal;
+		}
+		if(peso<pesominimo)
+		{
+			pesominimo=peso;
+			animalLiviano=animal;
+		}
+		else
+		{
+			if(peso>pesomaximo)
+			{
+				pesomaximo=peso;
+				animalPesado=animal;
+			}
 		}
 
-		
-	
-	
+		if(temperatura<0)
+		{
+			animalFrio++;
+		}
 
-		
+		if(contadorTemperatura==1)
+		{
+			temperaturaMinima=temperatura;
+			temperaturaMaxima=temperatura;
+		}
+
+			if(temperatura<temperaturaMinima)
+			{
+				temperaturaMinima=temperatura;
+			}
+		else{
+			if(temperatura>temperaturaMaxima)
+			{
+				temperaturaMaxima=temperatura
+			}
+		}
 
 
 
-		respuesta=prompt("Desea seguir sumando datos? si/no");
+
+
+
+
+
+
+
+
+
+
+
+		acumuladorPeso=acumuladorPeso+peso;
+		respuesta=prompt("Desea agregar otro animal");
 	}
-	acumuladorPeso=acumuladorPeso+peso;
-	promedioPeso=acumuladorPeso/contadorpeso;
+
+	
+	promedioPeso=acumuladorPeso/contadorPeso;
+
+
 
 	document.write("<br> La cantidad de temperaturas pares es: "+temperaturaPar);
 	document.write("<br> La cantidad de temperaturas impares es: "+temperaturaImpar);
-	document.write("<br> El nombre del animal más pesado es: "+nombremaximoP);
-	document.write("<br> El nombre del animal menos pesado es: "+nombreminimoP);
-	document.write("<br> El promedio de pesos es: "+promedioPeso);
-	document.write("<br>")
-	document.write("<br>")
+	document.write("<br> La cantidad de animales que viven en temperaturas bajo cero: "+animalFrio);
+	document.write("<br> El promedio de los pesos ingresados: "+promedioPeso);
+	document.write("<br> animal más pesado: "+animalPesado);
+	document.write("<br> animal más liviano: "+animalLiviano);
+	document.write("<br> temperatura minima: "+temperaturaMinima);
+	document.write("<br> temperatura maxima: "+temperaturaMaxima);
 
 }//FIN DE LA FUNCIÓN
